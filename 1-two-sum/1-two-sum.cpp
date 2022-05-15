dd8061b000b2291b6edc111a382bdf21c8de90e1
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-05-15 14:34:50 +0530

    Time: 22 ms (51.79%), Space: 11.2 MB (21.63%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -3,17 +3,18 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
         vector<int> ans;
+        map <int,int> mapp;
         
         for(int i=0; i<nums.size(); i++){
             
-            for(int j=i+1; j<nums.size(); j++){
+            if( mapp.find( target - nums[i] ) != mapp.end() ){
                 
-                if(nums[i] + nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
+                ans.push_back(mapp[target - nums[i]]);
+                ans.push_back(i);
+                break;
             }
+         
+            mapp[nums[i]] = i;       
         }
         return ans;
     }
