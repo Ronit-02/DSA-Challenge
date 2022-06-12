6bc6af58b3e24476181d8a644fd87b6f4a255331
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-12 14:05:58 +0530

    Time: 187 ms (10.98%), Space: 41.4 MB (56.31%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& arr, int target) {
+    int subarraySum(vector<int>& arr, int k) {
         
-        int n = arr.size();
+        int n = (int)arr.size();
         
         int ans = 0;
         
@@ -12,11 +12,11 @@ public:
         for(int i=0; i<n; ++i){
             sum += arr[i];              // increasing sum
             
-            if(sum == target)          // if sum == target
+            if(sum == k)          // if sum == target
                 ans++;
             // if (sum - target) is present, add its freq
-            if(hash.find(sum - target) != hash.end())    
-                ans += hash[sum - target];
+            if(hash.find(sum - k) != hash.end())    
+                ans += hash[sum - k];
             
             hash[sum]++;
         }
