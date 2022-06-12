e05c82a7402d75aa339de37b609dd2ce5cdbf7fe
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-12 14:04:14 +0530

    Time: 198 ms (8.43%), Space: 41.4 MB (48.82%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -4,23 +4,21 @@ public:
         
         int n = arr.size();
         
-        int sum = 0;
         int ans = 0;
+        
         unordered_map<int, int> hash;
-        hash[0] = 1;             // for 0 sum
+        int sum = 0;
         
         for(int i=0; i<n; ++i){
+            sum += arr[i];              // increasing sum
             
-            sum += arr[i];
-            int rsum = sum - target;    // remaining sum
+            if(sum == target)          // if sum == target
+                ans++;
+            // if (sum - target) is present, add its freq
+            if(hash.count(sum - target) == 1)    
+                ans += hash[sum - target];
             
-            if(hash.count(rsum) == 1)    // if rem_sum is there, increase ans
-                ans += hash[rsum];
-            
-            if(hash.count(sum) == 1)     // if it present -> increase freq
-                hash[sum]++;
-            else
-                hash[sum] = 1;          // if not -> give it freq = 1
+            hash[sum]++;
         }
         
         return ans;
