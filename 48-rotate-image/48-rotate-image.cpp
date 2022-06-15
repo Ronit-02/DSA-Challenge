0a036ded2a55fcaa4d213174cfe98cb3abe39223
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-16 00:53:16 +0530

    Time: 4 ms (39.26%), Space: 7.1 MB (73.20%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -4,6 +4,7 @@ public:
      
         int n = a.size();
         
+        // swapping non diagonal elements
         for(int i = 0; i < n; i++){
             for(int j = i+1; j < n; j++){
     
@@ -11,7 +12,7 @@ public:
             }
         }
         
-        int j = 0;
+        // swapping whole columns
         for(int j = 0; j < n/2; j++){
             for(int i = 0; i < n; i++){
             
