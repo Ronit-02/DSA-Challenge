da2c98ebcfaf5e6768b51cfd614f339b538a0437
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-16 22:53:04 +0530

    Time: 263 ms (61.12%), Space: 10.3 MB (70.54%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -1,24 +1,25 @@
-class Solution {
+class Solution 
+{
 public:
-    // Sieve of Eratosthenes
-    // Time complexity: O(n*log(logn))
-    
-    int countPrimes(int n) {
-        
-        vector<bool> prime(n+1, true);
-        prime[0] = prime[1] = false;    // as 1 and 2 are not prime numbers
-        
-        int cnt = 0;
-        
-        for(int i=2; i<n; i++){
-            
-            if(prime[i] == true){
-                cnt++;
-                    
-                for(int j=2*i; j<n; j = j+i)
-                    prime[j] = false;
+    int countPrimes(int n) 
+    {
+        std::vector<bool> table(n + 1, true);
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (table[i])
+            {
+                for (int p = i * i; p <= n; p += i)
+                {
+                    table[p] = false;
+                }
             }
         }
-        return cnt;
+        
+        int count = 0;
+        for (int i = 2; i < n; i++)
+        {
+            count += table[i];
+        }
+        return count;
     }
 };
