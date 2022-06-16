5d8b03dc6cf31ab9219998ba1c04efd794b72712
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-16 23:11:43 +0530

    Time: 15 ms (76.88%), Space: 10.6 MB (20.55%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -4,19 +4,20 @@ public:
         
         int n = nums.size();
         
-        vector<int> dp(n+1, INT_MAX);
-        dp[0] = INT_MIN;
+        vector<int> dp(n+1, INT_MAX);     // initializing all values to INT_MAX
+        dp[0] = INT_MIN;                  // but first value to INT_MIN
         
         for(int i=0; i < n; i++){
             
-            int idx = upper_bound(dp.begin(), dp.end(), nums[i]) - dp.begin();
+            // upper bound gives position of element just greater than searched element (nums[i])
+            int idx = upper_bound(dp.begin(), dp.end(), nums[i]) - dp.begin(); 
             
-            if(nums[i] > dp[idx-1] && nums[i] < dp[idx])
+            if(nums[i] > dp[idx-1] && nums[i] < dp[idx])      // verifying upper bound placement
                 dp[idx] = nums[i];
         }
         
         int len = 0;
-        for(int i=n; i >= 0; i--){    
+        for(int i=n; i >= 0; i--){      // last value ignoring all INT_MAX's is our ans
             if(dp[i] != INT_MAX){    
                 len = i;    
                 break;
