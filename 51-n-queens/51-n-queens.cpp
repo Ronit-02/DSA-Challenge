aaa564b8e8dd235e6fe0dc155bfdc09cea2d5faf
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-18 23:47:35 +0530

    Time: 19 ms (29.78%), Space: 7.3 MB (71.78%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -3,15 +3,22 @@ public:
     void filldfs(vector<vector<int>> &position, int n, int row, int col, int val){
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(i==row | j==col | i+j==row+col | i-j==row-col) position[i][j]+=val;
+                if(i==row | j==col | i+j==row+col | i-j==row-col) 
+                    position[i][j]+=val;
             }
         }
     }
     
-    void queens(vector<vector<string>> &result, int n, string &s, vector<string>& curr, vector<vector<int>> &position, int row_val){
-        if(row_val == n) {result.push_back(curr); return;}
+    void queens(vector<vector<string>> &result, int n, string &s, vector<string>& curr,                       vector<vector<int>> &position, int row_val){
+        
+        if(row_val == n){
+            result.push_back(curr); 
+            return;
+        }
         for(int i=0; i<n; i++){
+            
             if(position[row_val][i] == 0){
+                
                 s[i] = 'Q';
                 curr.push_back(s);
                 s[i] = '.';
@@ -22,14 +29,18 @@ public:
             }
         }
     }
-    
-    vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> solveNQueens(int n){
+        
         vector<vector<string>> result;
+        
         vector<vector<int>> position(n, vector<int> (n, 0));
         vector<string> curr;
         string s; 
-        for(int i=0; i<n; i++) s.push_back('.');
+        for(int i=0; i<n; i++)
+            s.push_back('.');
+        
         queens(result, n, s, curr, position, 0);
+        
         return result;
     }
 };
