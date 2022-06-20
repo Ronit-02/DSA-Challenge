7103dc76049f5230a8e7aef44bdc60ddcaf3e2ce
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-20 19:19:09 +0530

    Time: 38 ms (32.86%), Space: 23.3 MB (40.65%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -2,13 +2,18 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
      
-        int st = 0;
-        int e = s.size() - 1;
-        
-        while(st < e){
-            swap(s[st], s[e]);
-            st++;
-            e--;
-        }
+        stringReverse(s, 0, s.size()-1);
+    }
+    void stringReverse(vector<char> &str, int i, int j){
+
+        // Base Case
+        if(i > j)
+            return;
+
+        // Process
+        swap(str[i], str[j]);
+
+        //Recursive Call
+        stringReverse(str, i+1, j-1);
     }
 };
