41f9f7ebb81b118c95f81be614de1b590e71aa2c
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-20 12:48:20 +0530

    Time: 4 ms (52.75%), Space: 5.8 MB (80.03%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,13 +2,20 @@ class Solution {
 public:
     int fib(int n) {
         
-        // Base Case
-        if(n == 0) 
+        if(n == 0)
             return 0;
-        if(n == 1) 
+        if(n == 1)
             return 1;
         
-        // Recursive Call
-        return fib(n-1) + fib(n-2);
+        int a = 0;
+        int b = 1;
+        for(int i=3; i <= n; i++){
+            
+            int c = a+b;
+            a = b;
+            b = c;
+        }
+        
+        return a+b;
     }
 };
