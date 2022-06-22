7b0c4d925821070adce4ab1173777c321cfca4ba
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-23 00:32:20 +0530

    Time: 11 ms (13.74%), Space: 7.7 MB (61.01%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,35 +1,17 @@
 class Solution {
-public:
+    public:
+    
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
         
-        unordered_map<int, int> mp;
-        
-        // mapping values of arr2 to their respective position
-        for(int i=0; i<arr2.size(); i++)
-            mp[arr2[i]] = i;
-        
-        int n = arr1.size();
-        for(int i=0; i < n-1; i++){
-            
-            for(int j=i+1; j < n; j++){
-                
-                // if you can find both -> swap according to value
-                if(mp.find(arr1[i]) != mp.end() && mp.find(arr1[j]) != mp.end() 
-                   && mp[arr1[i]] > mp[arr1[j]])
-                    swap(arr1[i], arr1[j]);    
-                
-                // if you can find one that is present right side -> swap 
-                else if(mp.find(arr1[i]) == mp.end() && mp.find(arr1[j]) != mp.end())
-                    swap(arr1[i], arr1[j]);
-                
-                // if no one is present -> swap according to value
-                else if(mp.find(arr1[i]) == mp.end() && mp.find(arr1[j]) == mp.end() 
-                        && arr1[i] > arr1[j])
-                    swap(arr1[i], arr1[j]);  
-            }
-        }
-        
+        unordered_map<int,int> s;
+
+        for (int i=0; i<arr2.size(); i++)    // Hashmap of arr2
+            s[arr2[i]]=i;             
+
+        sort(begin(arr1), end(arr1), [&s](int a, int b) { 
+            return (s.count(a) && (s.count(b)==0 || s[a]<s[b])) || (s.count(b)==0 && a<b);
+        });        
+
         return arr1;
-        
     }
 };
