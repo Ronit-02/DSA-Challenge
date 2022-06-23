0f44f8271acaae384af2fd1578cbb772dcac1875
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-23 18:48:00 +0530

    Time: 11 ms (11.53%), Space: 16.8 MB (12.09%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void recurSubset(vector<int> nums, int idx, vector<int> subset, vector<vector<int>> &ans){
+        
+        // base case
+        if(idx == nums.size()){
+            ans.push_back(subset);
+            return;
+        }
+        
+        // excluding
+        recurSubset(nums, idx+1, subset, ans);
+        
+        // including
+        subset.push_back(nums[idx]);
+        recurSubset(nums, idx+1, subset, ans);
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+     
+        vector<vector<int>> ans;
+        vector<int> subset;
+        
+        int idx = 0;
+        recurSubset(nums, idx, subset, ans);
+        
+        return ans;
+    }
+};
