a1d9c3232cd008cd8473de8fd2c8c794b53bddc9
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-28 13:28:47 +0530

    Time: 115 ms (35.20%), Space: 17.3 MB (58.78%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -10,13 +10,16 @@ public:
         for(int i=0; i<n; i++)
             freq[s[i] - 'a']++;
         
+        // sorted, all freq are present at last
         sort(freq.begin(), freq.end());
         
+        // starting from second last
         for(int i=24; i>=0; i--){
             
-            if(freq[i] == 0) break;
+            if(freq[i] == 0) break;    // till we encounter 0
             
-            while(freq[i] >= freq[i+1] && freq[i] > 0){
+            // reduce till becomes smaller than prev, but not smaller than 0
+            while(freq[i] >= freq[i+1] && freq[i] > 0){   
                 freq[i]--;
                 ans++;
             }
