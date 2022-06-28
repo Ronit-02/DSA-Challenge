c6bdef789c4374307b29403f0d727d9768d871af
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-28 15:05:46 +0530

    Time: 36 ms (67.35%), Space: 27.1 MB (10.75%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -15,9 +15,9 @@ public:
                 if(cnt >= 1) 
                     return false;
                 
-                if(i == 1 || nums[i-2] <= nums[i]) 
+                if(i == 1 || nums[i-2] <= nums[i])    // case 1
                     nums[i-1] = nums[i];    
-                else 
+                else                                  // case 2
                     nums[i] = nums[i-1];
                 
                 cnt++;
