ffde3b800522c1ee5e0bba83ed81dec2a436d575
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-28 14:27:04 +0530

    Time: 45 ms (42.30%), Space: 28 MB (5.87%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& arr) {
+  
+        int n = arr.size();
+        vector<int> arr2(arr);
+        
+        int cnt1 = 0;
+        for(int i=n-2; i>=0; i--){
+            if(arr[i] > arr[i+1]){
+                arr[i] = arr[i+1];
+                cnt1++;
+            }
+        }
+        
+        int cnt2 = 0;
+        for(int i=1; i<n; i++){
+            if(arr2[i] < arr2[i-1]){
+                arr2[i] = arr2[i-1];
+                cnt2++;
+            }
+        }
+        
+        int cnt = min(cnt1, cnt2);
+        if(cnt <= 1)
+            return true;
+        else
+            return false;
+    }
+};
