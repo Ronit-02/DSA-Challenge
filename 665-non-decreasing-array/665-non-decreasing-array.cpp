af4e0e6fca37e13d077567a48cd3bb0ffe5aaaf5
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-28 14:28:29 +0530

    Time: 36 ms (67.35%), Space: 28 MB (5.87%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -5,18 +5,18 @@ public:
         int n = arr.size();
         vector<int> arr2(arr);
         
-        int cnt1 = 0;
-        for(int i=n-2; i>=0; i--){
-            if(arr[i] > arr[i+1]){
-                arr[i] = arr[i+1];
+        int cnt1 = 0;              // from starting to end
+        for(int i=1; i<n; i++){     
+            if(arr2[i] < arr2[i-1]){
+                arr2[i] = arr2[i-1];
                 cnt1++;
             }
         }
         
-        int cnt2 = 0;
-        for(int i=1; i<n; i++){
-            if(arr2[i] < arr2[i-1]){
-                arr2[i] = arr2[i-1];
+        int cnt2 = 0;              // from ending to start
+        for(int i=n-2; i>=0; i--){
+            if(arr[i] > arr[i+1]){
+                arr[i] = arr[i+1];
                 cnt2++;
             }
         }
