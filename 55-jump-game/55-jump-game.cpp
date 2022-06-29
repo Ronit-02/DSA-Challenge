902a2a8e75bedf270f6b7308b37573fe27e4e618
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-06-29 16:41:47 +0530

    Time: 75 ms (84.20%), Space: 48.4 MB (42.83%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
      
-        int current = 0;
-        int farthest = 0;
+        int current = 0;     // represents index after 1st, 2nd, .. jump
+        int farthest = 0;    // farthest distance from every index
         
         for(int i = 0; i < nums.size()-1; i++){
             
             farthest = max(farthest, nums[i] + i);
             
-            if(i == current)
+            if(i == current)   // exhausted every possibility from 1st, 2nd, ... jump
                 current = farthest;
         }
         
