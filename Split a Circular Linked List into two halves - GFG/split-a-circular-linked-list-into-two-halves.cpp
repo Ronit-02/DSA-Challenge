1ed9d0876d128ce12a84dedde0f0ac26a9fe124a
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-07-01 23:39:06 +0530

    Added solution - LeetHub

--- Split a Circular Linked List into two halves - GFG/split-a-circular-linked-list-into-two-halves.cpp
+++ Split a Circular Linked List into two halves - GFG/split-a-circular-linked-list-into-two-halves.cpp
@@ -101,37 +101,29 @@ struct Node
 
 void splitList(Node *head, Node **head1_ref, Node **head2_ref)
 {
-    Node *slow_ptr = head; 
-    Node *fast_ptr = head; 
+    Node *slow = head; 
+    Node *fast = head; 
       
     if(head == NULL) 
         return; 
       
-    /* If there are odd nodes in the circular list then 
-       fast_ptr->next becomes head and for even nodes 
-       fast_ptr->next->next becomes head */
-    while(fast_ptr->next != head && 
-          fast_ptr->next->next != head) 
-    { 
-        fast_ptr = fast_ptr->next->next; 
-        slow_ptr = slow_ptr->next; 
+    // fast -> next = head in odd nodes
+    // fast -> next -> next = head in even nodes
+    while(fast->next != head && fast->next->next != head) { 
+        fast = fast-> next-> next; 
+        slow = slow-> next; 
     } 
       
-    /* If there are even elements in list
-       then move fast_ptr */
-    if(fast_ptr->next->next == head) 
-        fast_ptr = fast_ptr->next; 
+    // even elements
+    if(fast-> next-> next == head) 
+        fast = fast-> next; 
           
-    /* Set the head pointer of first half */
     *head1_ref = head; 
           
-    /* Set the head pointer of second half */
-    if(head->next != head) 
-        *head2_ref = slow_ptr->next; 
+    if(head-> next != head) 
+        *head2_ref = slow-> next; 
           
-    /* Make second half circular */
-    fast_ptr->next = slow_ptr->next; 
+    fast-> next = slow-> next; 
           
-    /* Make first half circular */
-    slow_ptr->next = head;
+    slow-> next = head;
 }
