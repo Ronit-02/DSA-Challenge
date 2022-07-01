a5c743cebd501d0f055c97eb55fd9a942b09113f
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-07-01 18:39:53 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.09%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     void multiply(int a[3][3], int b[3][3]){
         
-        int mul[3][3];
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
+        int mul[3][3];      // we'll save multiplication in new matrix
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
                 
                 mul[i][j] = 0;
-                for (int k = 0; k < 3; k++)
+                for (int k=0; k<3; k++)
                     mul[i][j] += a[i][k]*b[k][j];
             }
         }
@@ -16,32 +16,34 @@ public:
             for (int j=0; j<3; j++)
                 a[i][j] = mul[i][j];  // Updating our matrix
     }
+    
+    // through binary exponentiation or power paradigm
     void power(int F[3][3], int n){
         
         int M[3][3] = {{1,1,1}, {1,0,0}, {0,1,0}};
 
-        if (n==1)
+        if (n == 1)
             return;
 
         power(F, n/2);
         multiply(F, F);
 
-        if (n % 2 != 0)   // if odd
+        if (n % 2 != 0)     // if n = odd
             multiply(F, M);
     } 
     int tribonacci(int n){
 
-        int F[3][3] = {{1,1,1}, {1,0,0}, {0,1,0}} ;
+        int F[3][3] = {{1,1,1}, {1,0,0}, {0,1,0}};    // as, a=1, b=1, c=1
 
-        //Base cases
+        // Base cases
         if(n==0)
             return 0;
         if(n==1 || n==2)
             return 1;
         
-        power(F, n-2);
+        power(F, n-2);      // we have to multiply (n-2) times
         
-        int ans = F[0][0] + F[0][1]; 
+        int ans = F[0][0] + F[0][1];       // F[0][0] * 1 + F[0][1] * 1 + F[0][2] * 0
         return ans;
     }
 };
