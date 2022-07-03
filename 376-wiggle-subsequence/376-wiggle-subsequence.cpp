578bd3b3b34b4c9622c7524c0ca2d23e99e87fee
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-07-03 19:36:30 +0530

    Time: 10 ms (30.89%), Space: 7 MB (75.45%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         
-        int inc = 1;
-        int dec = 1;
+        int inc = 1;    // we increase inc when increasing slope is found
+        int dec = 1;    // we increase dec when decreasing slope is found
         for(int i=1; i<nums.size(); i++){
             
-            if(nums[i-1] < nums[i])
-                inc = dec+1;
+            if(nums[i-1] < nums[i])    
+                inc = dec+1;             // as previous one was valley
             else if(nums[i-1] > nums[i])
-                dec = inc+1;
+                dec = inc+1;            // as previous one was mountain
         }
         
         return max(inc, dec);
