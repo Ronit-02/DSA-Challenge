41dc92e410316afe08880d846ba50a8ee812d85c
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-07-24 12:22:43 +0530

    Time: 32 ms (37.77%), Space: 24.1 MB (86.62%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // Using Morris Traversal
+    // TC: O(N)
+    // SC: O(1)
+    int kthSmallest(TreeNode* root, int k) {
+        
+        TreeNode *curr = root;
+        int ans = 0;
+        int i = 0;
+        
+        while(curr != NULL){
+            
+            // left subtree not present
+            if(curr -> left == NULL){
+                if(++i == k)
+                    ans = curr->val;
+                curr = curr -> right;
+            }
+            
+            // left subtree present
+            // point its predecessor (rightmost of left subtree)
+            else{
+                
+                TreeNode* prev = curr->left;
+                
+                while(prev -> right && prev -> right != curr){
+                    prev = prev->right;
+                }
+                // create link
+                if(prev -> right == NULL){
+                    prev -> right = curr;
+                    curr = curr -> left;
+                }
+                // remove link
+                else{
+                    if(++i == k)
+                        ans = curr->val;
+                    prev -> right = NULL;
+                    curr = curr -> right;
+                }
+            }
+        }
+        return ans;
+    }
+};
