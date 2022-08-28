566112337504f0a2671dd87b3310d2ccc14f5a8e
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-08-28 12:50:49 +0530

    Time: 40 ms (86.26%), Space: 12.2 MB (83.09%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,21 +1,17 @@
 class Solution {
 private:
     // Recursive Approach
-    void dfs(vector<vector<int>> &vis, vector<vector<char>>& grid, int i, int j){
+    void dfs(vector<vector<char>>& grid, int i, int j){
         
         // out of bounds
         if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j] == '0')
             return;
         
-        // already visited
-        if(vis[i][j] == 1)
-            return;
-        
-        vis[i][j] = 1;
-        dfs(vis, grid, i-1, j);    // up
-        dfs(vis, grid, i+1, j);    // down
-        dfs(vis, grid, i, j-1);    // left
-        dfs(vis, grid, i, j+1);    // right
+        grid[i][j] = '0';
+        dfs(grid, i-1, j);    // up
+        dfs(grid, i+1, j);    // down
+        dfs(grid, i, j-1);    // left
+        dfs(grid, i, j+1);    // right
     }
     
 public:
@@ -25,14 +21,14 @@ public:
         int n = grid[0].size();
         
         // visited matrix
-        vector<vector<int>> vis(m, vector<int> (n, 0));
+        // vector<vector<int>> vis(m, vector<int> (n, 0));
         int ans = 0;
         
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(vis[i][j] == 0 && grid[i][j] != '0'){
+                if(grid[i][j] != '0'){
                     ans++;
-                    dfs(vis, grid, i, j);
+                    dfs(grid, i, j);
                 }
             }
         }
