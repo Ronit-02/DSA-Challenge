d39c2530989e8926ef1a920d27734376997b549a
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-09-28 20:16:15 +0530

    Time: 84 ms (42.70%), Space: 20.4 MB (43.77%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -4,50 +4,62 @@ public:
         
         int n = dominoes.length();
         
-        vector<float> trav(n, 0);
-        float force = 1;
+        int *left = new int[n];
+        int nearestleftidx = -1;
         
-        // left traversal
-        for(int i=n-1; i>0; i--){
+        // left forces
+        for(int i=n-1; i>=0; i--){
             
-            if(dominoes[i] == '.' && trav[i] < 0){ 
-                trav[i-1] -= force/2;
-            }
             if(dominoes[i] == 'L'){
-                force = 1;
-                trav[i] -= force;
-                trav[i-1] -= force/2;
+                nearestleftidx = i;
             }
-            force /= 2;   // intensity reduces
+            else if(dominoes[i] == 'R'){ 
+                nearestleftidx = -1;
+            }
+            left[i] = nearestleftidx;
         }
         
-        force = 1;
+        int *right = new int[n];
+        int nearestrightidx = -1;
         
-        // right traversal
-        for(int i=0; i<n-1; i++){
+        // left forces
+        for(int i=0; i<n; i++){
             
-            if(dominoes[i] == '.' && trav[i] > 0){
-                trav[i+1] += force/2;
-            }
             if(dominoes[i] == 'R'){
-                force = 1;
-                trav[i] += force;
-                trav[i+1] += force/2;
+                nearestrightidx = i;
             }
-            force /= 2;   // intensity reduces
+            else if(dominoes[i] == 'L'){ 
+                nearestrightidx = -1;
+            }
+            right[i] = nearestrightidx;
         }
-        
-        // edge cases
-        if(dominoes[0] == 'L') trav[0] = -1;
-        if(dominoes[n-1] == 'R') trav[n-1] = 1;
             
         
         for(int i=0; i<n; i++){
-            if(trav[i] < 0) dominoes[i] = 'L';
-            else if(trav[i] == 0) dominoes[i] = '.';
-            else dominoes[i] = 'R';
+            
+            if(dominoes[i] == '.'){
+                    
+                if(right[i] == -1 && left[i] != -1)
+                    dominoes[i] = 'L';
+                else if(left[i] == -1 && right[i] != -1)
+                    dominoes[i] = 'R';
+                else if(right[i] == -1 && left[i] == -1)
+                    dominoes[i] = '.';
+                else{
+                    int leftdiff = abs(i - left[i]);
+                    int rightdiff = abs(i - right[i]);
+                    
+                    if(leftdiff == rightdiff)
+                        dominoes[i] = '.';
+                    else if(leftdiff < rightdiff)
+                        dominoes[i] = 'L';
+                    else
+                        dominoes[i] = 'R';
+                }
+            }
         }
         
         return dominoes;
     }
 };
+
