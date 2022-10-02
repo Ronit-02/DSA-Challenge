b8a03c6511e17b7b3681d86edd9aadb4ec2e4177
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-10-02 16:19:25 +0530

    Time: 11 ms (59.86%), Space: 11.1 MB (95.89%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,17 +1,41 @@
 class Solution {
-    int uf[26];
-    int find(int x) {
-        return uf[x] == x ? x : (uf[x] = find(uf[x]));
+    int parent[26];
+    int find(int node){
+        if(node == parent[node])
+            return node;
+        
+        return parent[node] = find(parent[node]);   // path compression
     }
 public:
     bool equationsPossible(vector<string>& equations) {
-        for (int i = 0; i < 26; ++i) uf[i] = i;
-        for (auto e : equations) {
-            if (e[1] == '=') uf[find(e[0] - 'a')] = find(e[3] - 'a'); 
+        
+        int n = equations.size();
+        
+        // 1. Establishing unions
+        for(int i=0; i<26; i++){
+            parent[i] = i;
         }
-        for (auto e : equations) {
-            if (e[1] == '!' && find(e[0] - 'a') == find(e[3] - 'a')) return false;
+        
+        for(int i=0; i<n; i++){
+            if(equations[i][1] == '='){
+                
+                int x = (equations[i][0] - 'a');
+                int y = (equations[i][3] - 'a');
+                parent[find(y)] = find(x);
+            }
         }
+        
+        // 2. finding inequalities
+        for(int i=0; i<n; i++){
+            if(equations[i][1] == '!'){
+                
+                int x = (equations[i][0] - 'a');
+                int y = (equations[i][3] - 'a');
+                if(find(y) == find(x))
+                    return false;
+            }
+        }
+        
         return true;
     }
 };
