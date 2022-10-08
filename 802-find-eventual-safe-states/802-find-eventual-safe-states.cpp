ec6a590602506e5236a1f4f7870d0abfe69ac8e0
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-10-08 11:38:26 +0530

    Time: 352 ms (37.32%), Space: 47.9 MB (60.75%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,39 +1,50 @@
 class Solution {
 public:
-    
-    bool dfs(int n,vector<bool>& vis,vector<bool>& path,vector<vector<int>>& adj){
-        
-        vis[n] = true;
-        path[n] = true;
-        
-        for (int node: adj[n]){
-            if (!vis[node]){
-                if (dfs(node,vis,path,adj))
+    // Intuition: (detect cycle)
+    // all the nodes which are part of a cycle or lead to a cycle 
+    // are not safe nodes, rest are!
+    bool dfs(vector<int> &vis, vector<int> &path, vector<vector<int>>& graph, int node){
+
+        vis[node] = 1;  // mark it
+        path[node] = 1;
+
+        // traversing all adjacent items
+        for(auto adjNode: graph[node]){
+
+            // 1. not visited, visit it
+            if(vis[adjNode] == 0){
+                if(dfs(vis, path, graph, adjNode) == true)  // if detected a cycle, break out
                     return true;
-            }else if (path[node]){
-                 return true;
+            }
+
+            // 2. visited but in same path
+            else if(path[adjNode] == 1){
+                return true;
             }
         }
-        path[n] = false;
+        path[node] = 0;   
         return false;
     }
-    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        
         int n = graph.size();
-        vector<bool> vis(n,false);
-        vector<bool> path(n,false);
         
-        for (int i=0;i<n;i++){
-             if (!vis[i]){
-                 dfs(i,vis,path,graph);
-             }
+        vector<int> vis(n, 0); // visited array
+        vector<int> path(n, 0);  // path-visited array
+
+        // for multi-component graph, need to check every node
+        for(int i=0; i<n; i++){
+            if(!vis[i])
+                dfs(vis, path, graph, i);
         }
         
-        vector<int> ans;
-        for (int i=0;i<n;i++){
-            if (path[i]==false)
-                ans.push_back(i);
+        vector<int> safe;
+
+        // all marked as path = 0, are safe
+        for(int i=0; i<n; i++){
+            if(path[i] == 0)
+                safe.push_back(i);
         }
-        return ans;
+        return safe;
     }
 };
