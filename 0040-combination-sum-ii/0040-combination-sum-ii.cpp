c85c36cfcb7a336002272015c3f6cbe757311c14
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-10-14 02:00:43 +0530

    Time: 7 ms (79.22%), Space: 11.5 MB (26.12%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    void recurComb(vector<int> &arr, int target, vector<vector<int>> &ans, vector<int> seq, int ind){
+        if(target == 0){
+            ans.push_back(seq);
+            return;
+        }   
+        
+        for(int i = ind; i < arr.size(); i++){
+            if(i > ind && arr[i] == arr[i-1])
+                continue;
+            if(arr[i] > target)
+                break;
+            
+            seq.push_back(arr[i]);
+            recurComb(arr, target - arr[i], ans, seq, i+1);
+            seq.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
+        vector<int> seq;
+        
+        recurComb(candidates, target, ans, seq, 0);
+        
+        return ans;
+    }
+};
