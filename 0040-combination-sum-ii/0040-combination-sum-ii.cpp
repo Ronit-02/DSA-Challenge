2b2fa15c8b890ccb2fb6f6ceac84a4334249a6c2
Ronit Khatri <71491311+Ronit-02@users.noreply.github.com>
2022-10-14 09:19:58 +0530

    Time: 9 ms (63.81%), Space: 11.4 MB (28.76%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,5 +1,9 @@
 class Solution {
 private:
+    // TC: O(2^t * k)
+    // Intuition: 
+    // to avoid picking up the same combnations i.e. we don't pick same element
+    // for certain kth position of a combination 
     void recurComb(vector<int> &arr, int target, vector<vector<int>> &ans, vector<int> seq, int ind){
         if(target == 0){
             ans.push_back(seq);
@@ -7,7 +11,7 @@ private:
         }   
         
         for(int i = ind; i < arr.size(); i++){
-            if(i > ind && arr[i] == arr[i-1])
+            if(i > ind && arr[i] == arr[i-1])  // skipping all the duplicates
                 continue;
             if(arr[i] > target)
                 break;
